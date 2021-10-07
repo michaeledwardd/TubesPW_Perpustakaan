c9a36ef21b9e81f2b091ae86966d5337a9a4ca96
akbarhlubis <alubis87@gmail.com>
2021-10-07 10:29:41 +0700

    Mengisi tampilan file login & register

---
+++ page/loginPage.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<!-- Catatan belum banyak yang di edit -->
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrit y="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="../style.css" rel="stylesheet">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-dark bg-dark fixed-top">
+        <div class="container">
+            <a class="navbar-brand fw-bold" href="\Tubes_PW\TubesPW_Perpustakaan">
+            <img src="../asset/icon/logo.svg" alt="Logo PerpustakanKu">
+            </a>
+        </div>
+    </nav>
+    <div class="bg bg-light text-dark">
+        <div class="container min-vh-100 d-flex align-items-center justify-content-center">
+            <div class="card text-dark bg-light ma-5 shadow" style="min-width: 25rem;">
+                <div class="card-header fw-bold">Login</div>
+                <div class="card-body">
+                    <form action="../process/loginProcess.php" method="post">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Username</label>
+                            <input class="form-control" id="username" name="username" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+
+                        <div class="d-grid gap-2">
+                            <button type="submit" class="btn btn-primary" name="login">Login</button>
+                        </div>
+                    </form>
+                    <p class="mt-2 mb-0">Belum punya akun ? <a href="./registerPage.php" class="textprimary">Klik Disini!</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
