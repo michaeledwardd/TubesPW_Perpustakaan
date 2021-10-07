e82aca1e23c24d35a65dd84da9087baea8ced331
akbarhlubis <alubis87@gmail.com>
2021-10-07 20:29:39 +0700

    Perbaikan Typo id

--- page/listUserPage.php
+++ page/listUserPage.php
@@ -2,7 +2,7 @@
     include '../component/sidebar.php'
 ?>
     <div class="container p-3 m-4 h-100" style="background-color: #FFFFFF; border-top: 5px solid #17337A; boxshadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" >
-        <h4 >Daftar Buku</h4>
+        <h4 >Daftar PENGGUNA</h4>
          <hr>
             <table class="table ">
             <thead>
