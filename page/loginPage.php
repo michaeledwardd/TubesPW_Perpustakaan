59191d7f6ab129076a95f6715c84fb8cb8eef735
user <michedwards0@gmail.com>
2021-10-13 13:07:22 +0700

    exception email sudah siap

--- page/loginPage.php
+++ page/loginPage.php
@@ -37,11 +37,11 @@
                     <form action="../process/loginProcess.php" method="post">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                            <input class="form-control" id="email" pattern="[^ @]*@[^ @]*" name="email" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <input type="password" class="form-control" id="password" name="password" required>
                         </div>
 
                         <div class="d-grid gap-2">
--- page/registerPage.php
+++ page/registerPage.php
@@ -36,11 +36,11 @@
                     <form action="../process/registerProcess.php" method="post">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Nama Pengguna</label>
-                            <input class="form-control" id="namaPengguna" name="namaPengguna" aria-describedby="emailHelp">
+                            <input class="form-control" id="namaPengguna" name="namaPengguna" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Nomor Identitas</label>
-                            <input class="form-control" id="nomorIdentitas" name="nomorIdentitas" aria-describedby="emailHelp">
+                            <input class="form-control" id="nomorIdentitas" name="nomorIdentitas" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Jenis Kelamin</label>
@@ -51,15 +51,15 @@
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input class="form-control" id="email" name="email" aria-describedby="emailHelp">
+                            <input class="form-control" pattern="[^ @]*@[^ @]*" id="email" name="email" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Username</label>
-                            <input class="form-control" id="username" name="username" aria-describedby="emailHelp">
+                            <input class="form-control" id="username" name="username" aria-describedby="emailHelp" required>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" name="password">
+                            <input type="password" class="form-control" id="password" name="password" required>
                         </div>
 
 
