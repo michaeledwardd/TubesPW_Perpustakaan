81daae6fd04c652d2cb8d13f477e3cbb03007170
user <michedwards0@gmail.com>
2021-10-21 13:13:39 +0700

    perubahan di edit peminjaman eror handling

--- process/editPeminjamanProcess.php
+++ process/editPeminjamanProcess.php
@@ -9,20 +9,29 @@
         $tanggalPeminjaman = $_POST['tanggalPeminjaman'];
         $tanggalPengembalian = $_POST['tanggalPengembalian'];
         
-
-        $queryEdit = mysqli_query($con, "UPDATE peminjaman SET namaPeminjam='$namaPeminjam', nomorIdentitas ='$nomorIdentitas', judulBuku='$judulBuku', tanggalPeminjaman='$tanggalPeminjaman', tanggalPengembalian='$tanggalPengembalian' WHERE id='$id'") or die(mysqli_error($con));                     
-        if($queryEdit){           
-            echo                 
-            '<script>                    
-                alert("Edit Success"); 
-                window.location = "../page/listPeminjamanPage.php"
-            </script>';        
-        }else{           
+        if($tanggalPengembalian < $tanggalPeminjaman)
+        {
+            echo '<script> alert ("Tanggal pengembalian tidak boleh kurang dari tanggal peminjaman"); window.history.back() </script>';
+        }
+        else
+        {
+            $queryEdit = mysqli_query($con, "UPDATE peminjaman SET namaPeminjam='$namaPeminjam', nomorIdentitas ='$nomorIdentitas', judulBuku='$judulBuku', tanggalPeminjaman='$tanggalPeminjaman', tanggalPengembalian='$tanggalPengembalian' WHERE id='$id'") or die(mysqli_error($con));                     
+            if($queryEdit)
+            {           
                 echo                 
                 '<script>                    
-                    alert("Edit Failed");                 
-                </script>';       
-        }   
+                    alert("Edit Success"); 
+                    window.location = "../page/listPeminjamanPage.php"
+                </script>';        
+            }
+            else
+            {           
+                    echo                 
+                    '<script>                    
+                        alert("Edit Failed");                 
+                    </script>';       
+            }   
+        }
     }
     else 
     {
