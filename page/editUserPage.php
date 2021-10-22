095786c3cfbea3bca6a7012d24023db6fe0ffb42
user <michedwards0@gmail.com>
2021-10-22 10:07:06 +0700

    benerin update user (passwordnya disabled aja)

--- page/editUserPage.php
+++ page/editUserPage.php
@@ -37,7 +37,7 @@ $data = mysqli_fetch_assoc($query);
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Username</label>
             <input type="text" class="form-control" id="username" name="username"
-                aria-describedby="emailHelp" value="<?php echo $data['username']; ?>" disabled>
+                aria-describedby="emailHelp" value="<?php echo $data['username']; ?>">
         </div>
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Email</label>
@@ -48,7 +48,7 @@ $data = mysqli_fetch_assoc($query);
         <div class="mb-3">
             <label for="exampleInputEmail1" class="form-label">Password</label>
             <input type="password" class="form-control" id="password" name="password"
-                aria-describedby="emailHelp" value="<?php echo $data['password']; ?>">
+                aria-describedby="emailHelp" value="<?php echo $data['password']; ?>" disabled>
         </div>
 
 
