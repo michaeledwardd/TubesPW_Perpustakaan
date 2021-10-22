f84539093545c20fd27501192763de07c0421ea0
akbarhlubis <alubis87@gmail.com>
2021-10-22 15:33:22 +0700

    memperbaiki index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container">
       <div class="logo">
-        <a class="navbar-brand fw-bold" >
+        <a class="navbar-brand fw-bold">
           <img src="asset/icon/logo.svg" alt="Logo PerpustakanKu">
         </a>
       </div>
@@ -54,13 +54,13 @@
     <div class="container">
       <div class="row outline outline-primary mt-5">
         <div class="col-md mt-4">
-        <h1 class="display-5">Baca Buku Tanpa Ribet!</h1>
-            <p>Koleksi kami <strong><mark class="bg-warning"> lengkap dan mudah</mark></strong> untuk dicari tanpa perlu sulit dan ribet untuk dicari.</p>
-            <hr class="my-4">
-            <a class="btn btn-primary btn-lg" href="./page/registerPage.php" role="button"><strong>Daftar Sekarang!</strong> </a>
+          <h1 class="display-5">Baca Buku Tanpa Ribet!</h1>
+          <p>Koleksi kami <strong><mark class="bg-warning"> lengkap dan mudah</mark></strong> untuk dicari tanpa perlu sulit dan ribet untuk dicari.</p>
+          <hr class="my-4">
+          <a class="btn btn-primary btn-lg" href="./page/registerPage.php" role="button"><strong>Daftar Sekarang!</strong> </a>
         </div>
         <div class="col-md mt-4">
-        <img width="90%" src="asset/icon/lemari_buku.svg" alt="Lemari Buku">
+          <img width="90%" src="asset/icon/lemari_buku.svg" alt="Lemari Buku">
         </div>
       </div>
     </div>
@@ -69,75 +69,78 @@
 
   <!-- Layanan Kami -->
   <section class="page-section bg-light" id="services">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,160L48,181.3C96,203,192,245,288,250.7C384,256,480,224,576,186.7C672,149,768,107,864,128C960,149,1056,235,1152,240C1248,245,1344,171,1392,133.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-            <div class="container">
-                <div class="text-center">
-                    <h1 class="section-heading text-uppercase mb-4">Layanan Kami</h1>
-                </div>
-                <div class="row text-center">
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-warning"></i>
-                            <i class="bi bi-skip-forward-circle-fill  fa-stack-2x text-light"></i>
-                        </span>
-                        <h4 class="my-3">Cepat, Mudah & Instan</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-warning"></i>
-                            <i class="bi bi-stars fa-stack-2x text-light"></i>
-                        </span>
-                        <h4 class="my-3">Berkualitas</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                    </div>
-                    <div class="col-md-4">
-                        <span class="fa-stack fa-4x">
-                            <i class="fas fa-circle fa-stack-2x text-warning"></i>
-                            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
-                        </span>
-                        <h4 class="my-3">Legal dan Aman</h4>
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.</p>
-                    </div>
-                </div>
-            </div>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,138.7C640,160,800,224,960,250.7C1120,277,1280,267,1360,261.3L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
-        </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,160L48,181.3C96,203,192,245,288,250.7C384,256,480,224,576,186.7C672,149,768,107,864,128C960,149,1056,235,1152,240C1248,245,1344,171,1392,133.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+    </svg>
+    <div class="container">
+      <div class="text-center">
+        <h1 class="section-heading text-uppercase mb-4">Layanan Kami</h1>
+      </div>
+      <div class="row text-center">
+        <div class="col-md-4">
+          <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-warning"></i>
+            <i class="bi bi-skip-forward-circle-fill  fa-stack-2x text-light"></i>
+          </span>
+          <h4 class="my-3">Cepat, Mudah & Instan</h4>
+          <p class="text-muted">Pelayanan cepat, mudah, dan instan!.</p>
+        </div>
+        <div class="col-md-4">
+          <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-warning"></i>
+            <i class="bi bi-stars fa-stack-2x text-light"></i>
+          </span>
+          <h4 class="my-3">Berkualitas</h4>
+          <p class="text-muted">Menyediakan Buku yang berkualitas!.</p>
+        </div>
+        <div class="col-md-4">
+          <span class="fa-stack fa-4x">
+            <i class="fas fa-circle fa-stack-2x text-warning"></i>
+            <i class="fas fa-lock fa-stack-1x fa-inverse"></i>
+          </span>
+          <h4 class="my-3">Legal dan Aman</h4>
+          <p class="text-muted">Buku yang tersedia memiliki sertifikasi legal dan aman digunakan.</p>
+        </div>
+      </div>
+    </div>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffffff" fill-opacity="1" d="M0,160L80,149.3C160,139,320,117,480,138.7C640,160,800,224,960,250.7C1120,277,1280,267,1360,261.3L1440,256L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+    </svg>
+  </section>
   <!-- Layanan Kami End -->
-  
+
   <!-- Buku Kami -->
   <section class="py-5">
-            <div class="container px-4 px-lg-5">
-            <h1 class="section-heading text-uppercase mb-4 text-center">Top Book of the week</h1>
-                <div class="row gx-4 gx-lg-5 align-items-center mb-5">
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://images-na.ssl-images-amazon.com/images/I/41c62f1VaYL._SX258_BO1,204,203,200_.jpg" alt="..." /></div>
-                    <div class="col-md-6">
-                        <h1 class="display-5 fw-bolder">UI/UX Design Sketchbook</h1>
-                        
-                        <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
-                        <div class="d-flex">
-                        </div>
-                    </div>
-                </div>
-                <div class="row gx-4 gx-lg-5 align-items-center mb-5">
-                  <div class="col-md-6">
-                      <h1 class="display-5 fw-bolder">Bootstrap 4: Designing Awesome Responsive Website</h1>
-                      
-                      <p class="p">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium at dolorem quidem modi. Nam sequi consequatur obcaecati excepturi alias magni, accusamus eius blanditiis delectus ipsam minima ea iste laborum vero?</p>
-                      <div class="d-flex">
-                      </div>
-                  </div>
-                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://elibrary.bsi.ac.id/assets/images/buku/206355.png" alt="..." /></div>
-                </div>
-            </div>
-        </section>
+    <div class="container px-4 px-lg-5">
+      <h1 class="section-heading text-uppercase mb-4 text-center">Top Book of the week</h1>
+      <div class="row gx-4 gx-lg-5 align-items-center mb-5">
+        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://images-na.ssl-images-amazon.com/images/I/41c62f1VaYL._SX258_BO1,204,203,200_.jpg" alt="..." /></div>
+        <div class="col-md-6">
+          <h1 class="display-5 fw-bolder">UI/UX Design Sketchbook</h1>
+
+          <p class="p">Buku ini menjelaskan tentang bagaimana menjadi UX/UI Designer yang baik serta tips dan trik dalam menghadapi segala urusan terkait hal tersebut</p>
+          <div class="d-flex">
+          </div>
+        </div>
+      </div>
+      <div class="row gx-4 gx-lg-5 align-items-center mb-5">
+        <div class="col-md-6">
+          <h1 class="display-5 fw-bolder">Bootstrap 4: Designing Awesome Responsive Website</h1>
+
+          <p class="p">Buku ini merupakan buku pegangan tentang Bootstrap 4 yang dapat digunakan oleh siapa saja yang ingin mendalami bidang keahlian web design. </p>
+          <div class="d-flex">
+          </div>
+        </div>
+        <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://elibrary.bsi.ac.id/assets/images/buku/206355.png" alt="..." /></div>
+      </div>
+    </div>
+  </section>
   <!-- Buku Kami End -->
 
   <!-- Carousel -->
   <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="container">
-    <h1 class="section-heading text-uppercase mb-4 text-center">PerpustakaanKita Gallery</h1>
-
+      <h1 class="section-heading text-uppercase text-center">PerpustakaanKita Gallery</h1>
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="https://images.unsplash.com/photo-1524311583145-d5593bd3502a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80" class="d-block w-100" alt="...">
@@ -150,18 +153,20 @@
         </div>
       </div>
     </div>
-</div>
+  </div>
   <!-- Carousel End -->
 
-  <br>
-  <center>
-    <h3>Kunjungi Perpustakaan kami di: </h3>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.1350320383776!2d110.41304270574608!3d-7.780808838571881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59f1d2361f71%3A0x4a2ce83adbcfd5aa!2sPerpustakaan%20Universitas%20Atma%20Jaya%20Yogyakarta!5e0!3m2!1sid!2sid!4v1633610599589!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-    </center>
-      <br>
-  
+  <!-- Peta Lokasi -->
+  <div class="container my-5">
+    <div class="text-center">
+      <h1>Kunjungi Perpustakaan Kami di:</h1>
+      <iframe title="Peta" class="container" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3324.1350320383776!2d110.41304270574608!3d-7.780808838571881!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a59f1d2361f71%3A0x4a2ce83adbcfd5aa!2sPerpustakaan%20Universitas%20Atma%20Jaya%20Yogyakarta!5e0!3m2!1sid!2sid!4v1633610599589!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+  </div>
+  <!-- Peta Lokasi End -->
+
   <!-- Footer -->
-  <div class="bg-dark text-light text-center mt-5">
+  <div class="bg-dark text-light text-center">
     <div class="card-header">
       Copyright © 2021 | Tugas Besar PW
     </div>
@@ -173,7 +178,7 @@
     </div>
   </div>
   <!-- Footer End -->
-  
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
