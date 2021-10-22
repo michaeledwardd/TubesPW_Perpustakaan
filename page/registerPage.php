6ede48e5323ac00b8da4821a6ce1eae6058e8fad
akbarhlubis <alubis87@gmail.com>
2021-10-22 20:16:46 +0700

    nambah

--- page/registerPage.php
+++ page/registerPage.php
@@ -7,6 +7,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,  initial-scale=1">
     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integ- rity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="../style.css" rel="stylesheet">
     <title>Register Page</title>
